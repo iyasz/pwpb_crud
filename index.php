2742964262db53d4d2ecace5b00403bffecaef67
Yasz Avellia <zakamaragames@gmail.com>
2022-10-05 10:11:13 +0700

    mencoba grid

--- index.php
+++ index.php
@@ -13,18 +13,32 @@ include "koneksi.php";
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   <link rel="stylesheet" href="app/style.css">
   <title>Home - Yasz Storage Home</title>
 </head>
 
 <body>
   <style>
+    .container {
+      position: relative;
+      top: 120px;
+    }
+
     .na {
       position: fixed;
       top: 0;
       right: 0;
       z-index: 3;
       left: 0;
+    }
+
+    .card {
+      border: none;
+    }
+
+    .bg-pr {
+      background-color: #404142;
     }
   </style>
   <!-- header -->
@@ -41,6 +55,7 @@ include "koneksi.php";
           <a class="nav-link" href="supplier/">Supplier</a>
           <a class="nav-link" href="transaksi/">Transaksi</a>
           <a class="nav-link" href="admin/">Admin</a>
+
         </div>
       </div>
     </div>
@@ -51,9 +66,36 @@ include "koneksi.php";
   <div class="container">
     <div class="row">
       <div class="col-lg-12">
-        <div class="card">
-          <div class="card-body">
-
+        <div class="card shadow-sm">
+          <div class="card-body d-flex">
+            <div class="col-md-3 bg-pr d-flex rounded-3">
+              <i class='bx bx-user-pin p-4 bx-md bg-danger rounded-circle'></i>
+              <div class="ms-3">
+                <h5>Jumlah Admin</h5>
+                <p>15</p>
+              </div>
+            </div>
+            <div class="col-md-3 bg-pr d-flex rounded-3">
+              <i class='bx bx-user-pin p-4 bx-md bg-danger rounded-circle'></i>
+              <div class="ms-3">
+                <h5>Jumlah Admin</h5>
+                <p>15</p>
+              </div>
+            </div>
+            <div class="col-md-3 bg-pr d-flex rounded-3">
+              <i class='bx bx-user-pin p-4 bx-md bg-danger rounded-circle'></i>
+              <div class="ms-3">
+                <h5>Jumlah Admin</h5>
+                <p>15</p>
+              </div>
+            </div>
+            <div class="col-md-3 bg-pr d-flex rounded-3">
+              <i class='bx bx-user-pin p-4 bx-md bg-danger rounded-circle'></i>
+              <div class="ms-3">
+                <h5>Jumlah Admin</h5>
+                <p>15</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
